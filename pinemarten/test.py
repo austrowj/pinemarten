38f37a67cc97ccd2b2608b9a86fa18fc8e25c530
austrowj <austrowj@users.noreply.github.com>
2024-08-14 15:37:53 -0400

    Moved product names to config file.

--- pinemarten/test.py
+++ pinemarten/test.py
@@ -4,10 +4,11 @@ import requests, json, sys
 with open('api/config.json') as f:
     config = json.load(f)
 
-endpoint = sys.argv[1]
+product = config['products'][sys.argv[1]]
+endpoint = sys.argv[2]
 
 response = requests.get(
-    config['url_root'] + config['product'] + endpoint,
+    config['url_root'] + product + endpoint,
     headers = config['headers']
 )
 print(json.dumps(json.loads(response.content.decode('utf-8')), indent=4))

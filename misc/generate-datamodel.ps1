03db2844240ae449cec52887c79a619e25bbe921
austrowj <austrowj@users.noreply.github.com>
2024-08-16 13:25:00 -0400

    Testing model validation - it works!

--- misc/generate-datamodel.ps1
+++ misc/generate-datamodel.ps1
@@ -1,1 +1,7 @@
-datamodel-codegen --input .\api\cdisc-library-api.json --input-file-type openapi --output .\api\cdisc_library_api_schema.py --use-annotated
+datamodel-codegen `
+    --input .\api\cdisc-library-api.json `
+    --input-file-type openapi `
+    --output .\pinemarten\schemas\cdisc_library_api_schema.py `
+    --base-class pydantic_v2.BaseModel `
+    --use-annotated `
+    --reuse-model `
--- pinemarten/pydantic_test.py
+++ pinemarten/pydantic_test.py
@@ -1,5 +1,11 @@
 # pyright: strict
 
-from pydantic import Field
+import json, sys
+from schemas import cm_variables as cm
 
-f = Field()
+x = cm.Model.model_validate(json.load(open(sys.argv[1])))
+
+for y in x.field_links.datasetVariables:
+    print(f"{y.href.split('/')[-1]}\t{y.title}")
+
+#print(x.model_dump_json(indent=4))

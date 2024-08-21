406e8b238a3a03b5d520339e5ef1d426b277b080
austrowj <austrowj@users.noreply.github.com>
2024-08-21 15:26:48 -0400

    Now uses data integrity utility function, plus more robust file output options.

--- pinemarten/dataset_generator.py
+++ pinemarten/dataset_generator.py
@@ -2,46 +2,66 @@
 
 # Generates a python file that defines a Pydantic class based on a dataset from the CDISC API.
 
-import logging, jinja2, datetime
-from dataclasses import dataclass
+import logging, jinja2, datetime, textwrap, json
+from pathlib import Path
 from typing import List
+from dataclasses import dataclass
 
 from cdisc_library_api_client.types import Unset
 from cdisc_library_api_client.models.sdtmig_dataset import SdtmigDataset
-#from cdisc_library_api_client.models.ig_variable_attributes import IgVariableAttributes
+
+from util import ensure
 
 @dataclass
 class OutputVariable:
     name: str
     python_type: str
 
-def generate(ds: SdtmigDataset, output_root: str) -> None:
-    if isinstance(ds.dataset_variables, Unset):
-        raise Exception('Dataset does not have variable attributes set.')
+def generate(ds: SdtmigDataset, out_path: Path | None) -> None:
+    dataset_variables = ensure(ds.dataset_variables)
     
     last = 0
     out_vars: List[OutputVariable] = []
-    for variable in ds.dataset_variables:
-        if isinstance(variable.ordinal, Unset):
-            raise Exception('Variable does not have an ordinal value: ' + str(variable.name))
-        if isinstance(variable.name, Unset):
-            raise Exception('Variable does not have a name: ' + str(variable.name))
-        
-        ordinal = int(variable.ordinal)
+    for variable in dataset_variables:
+
+        name = ensure(variable.name, f'No name present for ordinal {variable.ordinal}.')
+        ordinal = int(ensure(variable.ordinal, f'No ordinal present for variable {name}'))
+
         if ordinal <= last:
             logging.warning('Dataset variables not processed in ordinal order: ' + str(variable.name))
 
         out_vars.append(OutputVariable(
-            name=variable.name.ljust(8),
+            name=name.ljust(8),
             python_type= 'str' if variable.simple_datatype == 'Char' else 'float'
         ))
         last = ordinal
     
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('pinemarten/templates'))
     template = env.get_template('ds.py.jinja')
-    print(template.render(
+    output = template.render(
         generator={'name': __name__},
         input={'name': str(ds.name) + '.json'},
         output={'time': datetime.datetime.now()},
-        dataset={'name': ds.name, 'variables': out_vars}
-    ))
+        dataset={
+            'name': ds.name,
+            'label': ds.label,
+            'description':
+                [] if isinstance(ds.description, Unset)
+                else textwrap.fill(ds.description, width=72-4).split(sep='\n'),
+            'dataset_structure':
+                [] if isinstance(ds.dataset_structure, Unset)
+                else textwrap.fill(ds.dataset_structure, width=72-4).split(sep='\n'),
+            'variables': out_vars
+        }
+    )
+
+    if out_path:
+        
+        with open(out_path / f'{str(ds.name)}.json', 'w') as f:
+            json.dump(ds.to_dict(), f, indent=4)
+
+        with open(out_path / f'{str(ds.name)}.py', 'w') as f:
+            f.write(output)
+            
+    else:
+        print(output)
--- pinemarten/generated_api_test.py
+++ pinemarten/generated_api_test.py
@@ -1,21 +1,18 @@
 # pyright: strict
 
 import json, sys
-import dataset_generator
+from pathlib import Path
 
 from cdisc_library_api_client.api.sdtm_implementation_guide_sdtmig import api_products_sdtmig_get_datasets
 from cdisc_library_api_client.api.sdtm_implementation_guide_sdtmig import api_products_sdtmig_get_dataset
 #from cdisc_library_api_client.api.sdtm_implementation_guide_sdtmig import api_products_sdtmig_get_dataset_variable
 
-from cdisc_library_api_client.models.data_tabulation_dataset_list import DataTabulationDatasetList
-from cdisc_library_api_client.models.data_tabulation_dataset_list_links import DataTabulationDatasetListLinks
-from cdisc_library_api_client.models.sdtmig_dataset import SdtmigDataset
-
 from cdisc_library_api_client.client import Client
 
-import endpoint_hack
+import endpoint_hack, dataset_generator
+from util import ensure
 
-def run(out_root: str=''):
+def run(version: str, out_root: str=''):
 
     # Set up connection with secret API info
     with open('api/config.json') as f:
@@ -29,58 +26,52 @@ def run(out_root: str=''):
 
     with client as client:
         # Obtains the list of all SDTMIG datasets for a hardcoded version.
-        dataset_data = api_products_sdtmig_get_datasets.sync(version="3-4", client=client)
+        dataset_data = api_products_sdtmig_get_datasets.sync(version=version, client=client)
+        dataset_data = ensure(dataset_data)
 
-        # Silently do nothing if anything unexpected happens ¯\_(ツ)_/¯
-        if not isinstance(dataset_data, DataTabulationDatasetList): return
-
-        # The dataset is valid; print to file if folder specified, otherwise to stdout.        
         if out_root:
-            with open(f'{out_root}/datasets.json','w') as f:
+            out_path = Path(out_root, version)
+            out_path.mkdir(parents=True, exist_ok=True)
+        else:
+            out_path = None
+
+        # Print to file if folder specified, otherwise to stdout.        
+        if out_path:
+            with open(out_path / 'datasets.json', 'w') as f:
                 json.dump(dataset_data.to_dict(), f, indent=4)
         else:
             print(json.dumps(dataset_data.to_dict(), indent=4))
 
-
-        if not isinstance(dataset_data.field_links, DataTabulationDatasetListLinks): return
-        if not isinstance(dataset_data.field_links.datasets, list): return
+        links = ensure(dataset_data.field_links)
+        datasets = ensure(links.datasets)
 
         # Fetch the details of each dataset returned
-        for i, dataset_link in enumerate(dataset_data.field_links.datasets):
-            # but only the first two :)
-            if i > 0: break
-            print(dataset_link)
+        for i, dataset_link in enumerate(datasets):
+            # but only for selected ones :)
+            if i not in (0,1,2): continue
 
-            # Another silent fail
-            if not isinstance(dataset_link.href, str):
-                print('href is not a str :<')
-                continue
+            print(dataset_link)
+            href = ensure(dataset_link.href)
 
             # Trying out a pattern (hack) to extract endpoint arguments.
             # If we assume the type of object this link returns, we can parse the argument values out of its href.
             # Yes, we are using a private member function from the generated client. May end up regretting this later.
             parsed_args = endpoint_hack.parse_endpoint_arguments(
-                dataset_link.href,
+                href,
                 api_products_sdtmig_get_dataset,
                 'version', 'dataset'
             )
 
-            # Make and "validate" the request
+            # Make and validate the request
             res = api_products_sdtmig_get_dataset.sync(**parsed_args, client=client)
-            if not isinstance(res, SdtmigDataset):
-                print("res is not an SdtmigDataset :<")
-                continue
+            res = ensure(res)
 
             # Print the dataset info (optionally to file) if it's valid
-            if out_root:
-                with open(f'{out_root}/{parsed_args["dataset"]}.json','w') as f:
-                    json.dump(res.to_dict(), f, indent=4)
-            else:
-                dataset_generator.generate(res, '')
+            dataset_generator.generate(res, out_path)
 
         # Old code from the example, left here for reference
 
         # or if you need more info (e.g. status_code)
         #response: Response[MyDataModel] = get_my_data_model.sync_detailed(client=client)
 
-if __name__ == '__main__': run(sys.argv[1] if len(sys.argv) > 1 else '')
+if __name__ == '__main__': run(sys.argv[1], *sys.argv[2:] if len(sys.argv) > 2 else '')

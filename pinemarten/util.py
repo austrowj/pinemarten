089609e6bd27e3bf7815f4d2e945bf517fafd3c3
austrowj <austrowj@users.noreply.github.com>
2024-08-21 15:18:42 -0400

    Now handles API errors as well.

--- pinemarten/util.py
+++ pinemarten/util.py
@@ -1,18 +1,22 @@
 # pyright: strict
 
+import json
 from typing import TypeVar
 from cdisc_library_api_client.types import Unset
+from cdisc_library_api_client.models.error import Error
 
 T = TypeVar('T')
 
-def ensure(x: T | Unset | None, msg: str = '') -> T:
+def ensure(x: T | Unset | Error | None, msg: str = '') -> T:
 
     if msg=='':
-        msg = f'Object validation failed, expected {T}.'
+        msg = f'Object validation failed.'
 
-    if x is None:
-        raise Exception(msg + ' (was None)')
     if isinstance(x, Unset):
         raise Exception(msg + ' (was Unset)')
+    if isinstance(x, Error):
+        raise Exception(msg + f'\n Error: {json.dumps(x.to_dict(), indent=4)}')
+    if x is None:
+        raise Exception(msg + ' (was None)')
     
     return x

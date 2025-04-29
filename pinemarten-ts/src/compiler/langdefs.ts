d151d6c43f65b3bf87971d6b52c85e6e81e97fae
austrowj <austrowj@users.noreply.github.com>
2025-04-29 16:59:20 -0400

    Add test for full table transform.

--- pinemarten-ts/src/compiler/langdefs.ts
+++ pinemarten-ts/src/compiler/langdefs.ts
@@ -69,9 +69,20 @@ const df2 = df
     .with(x => ({test: x.n * 0 as 0}))
 ;
 df2.schema;
+test_transform(df2.with(x => ({id: 1})).schema); // only permitted with fields of the expected name and type
 
 df2.whereEq('n', 1).schema; // type of n is now literal '1'
 df2.where(x => x.n == 1 && x.mystr.endsWith('.xlsx')).schema; // column schema is unchanged
 
 df.with(x => ({id2: x.id, mystr: ''})).join_on(df2, 'id2').schema;
 df.join(df2, 'id', 'test').schema;
+
+function test_table_transform<T extends {id?: number, id2?: number}>(df: T) {
+    return {
+        id: df.id !== undefined ? df.id : -1,
+        id2: df.id2 !== undefined ? df.id2 : '',
+        ...df
+    }
+}
+
+df.as(test_table_transform).schema; // works!

1d9540affd1148834a81e9085c0e3dde1dd57269
austrowj <austrowj@users.noreply.github.com>
2025-04-30 11:53:35 -0400

    Add notes about data-mask compilation ideas.

--- pinemarten-ts/src/compiler/langdefs.ts
+++ pinemarten-ts/src/compiler/langdefs.ts
@@ -11,6 +11,20 @@ class Dataframe<T> {
     /* TODO: this API is not friendly for preserving column schema guarantees. */
 
     /*
+        Both 'as' and 'with' compile into a mutate call, which means their argument has to be translated to a
+        dplyr data-mask by the compiler.
+        Current idea: compiler will accept only a very specific AST head:
+            - Arrow function
+            - Single argument
+            - Body is exactly a parenthesized expression containing an object literal, and nothing else
+            - Compiler will replace references to the argument using the ".data" syntax
+        This means that named functions can't be used even though it results in valid Typescript :/ but a
+        function that accepts a Dataframe and calls "as"/"with" on it is okay.
+        
+        I guess this behavior matches that you cannot pass "mutate" a function that returns a data-mask.
+    */
+
+    /*
         Selects *only* the specified columns.
         Can include arbitrary expressions involving existing columns.
     */

e20eb3caee372140d6e6285de455b30beb53faec
austrowj <austrowj@users.noreply.github.com>
2025-05-01 16:51:28 -0400

    Restore a few other examples that broke earlier.

--- pinemarten-ts/test/langtest.R.ts
+++ pinemarten-ts/test/langtest.R.ts
@@ -7,11 +7,11 @@ function test(df: Dataframe<{ id: number, name: string, zz: boolean }>) {
     function test_transform<T extends { id: number }>(x: T) { return x.id + 1 }
     const count = 3;
 
-    count.toLocaleString()//.repeat(3*(1+2));
+    count.toLocaleString().repeat(3*(1+2));
 
     const df2 = df
         .as(x => ({
-            id2: 3,//test_transform(x),
+            id2: test_transform(x),
             n: x.name,
             mystr: x.name.repeat(count),
             zz: x.zz

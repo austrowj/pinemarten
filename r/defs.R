40756a16791f7f17bdef511d5229e1375c4a4185
austrowj <austrowj@users.noreply.github.com>
2025-05-09 23:53:05 -0400

    Implement new augment. Still need to test literally all of the R code.

--- r/defs.R
+++ r/defs.R
@@ -12,21 +12,20 @@ rename <- function(df, rename_specification) {
     # The rename specification is originally an object literal. In R it is a list.
     # The properties of the object are the new names and the values are the old names.
     spec_symbols <- lapply(rename_specification, function(old) expr(!!dplyr::sym(old))) # Only necessary to make .keep work.
-    mutate(!!!spec_symbols, .keep = 'unused')
+    dplyr::mutate(!!!spec_symbols, .keep = 'unused')
 }
 
-augment <- function(df, augment_specification) {
+augment <- function(df, col_name, f, argument_binding) {
+    # argument_binding is just a named list whose names are the parameters of f and values are the columns of df to use.
+    new_column <- lapply(lst, function(colname) df[[colname]]) |> f()
+    df[col_name] <- new_column
+    df
+}
+
+augment_bad <- function(df, augment_specification) {
     # The augment specification is an object literal. In R it is a list.
     # The properties of the object are the new column names and the values are the functions to compute those columns.
     # Mutate() itself ensures that the rows are unaffected apart from adding the new columns.
     computed_cols <- lapply(spec, function(f) f(df))
-    mutate(!!!computed_cols, .keep = 'all')
-}
-
-# TODO: remove, we have to handle this at the compiler level.
-as <- function(df, transform) {
-    # Apply the given transformation to the dataframe and return result.
-    # df: the starting dataframe.
-    # transform: a function that returns a new dataframe.
-    transform(df)
+    df |> dplyr::mutate(!!!computed_cols, .keep = 'all')
 }

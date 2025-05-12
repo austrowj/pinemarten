f3e09bfe67655827ae726f33836741697cc7aed7
austrowj <austrowj@users.noreply.github.com>
2025-05-12 13:17:58 -0400

    Add option to output file.

--- pinemarten-ts/src/compiler/compile.ts
+++ pinemarten-ts/src/compiler/compile.ts
@@ -1,14 +1,14 @@
 import { SimplifyingTransformer } from './transformer_simplify';
 import { RTransformer } from './transformer_to_r';
 import { printR } from './printer';
+import * as fs from 'fs';
 
 function compile(filename: string) {
     const simpleAst = new SimplifyingTransformer(filename).getAST();
     const rAst = new RTransformer(simpleAst).getAST();
-
-    console.log(JSON.stringify(rAst, null, 2));
-    console.log('\nGenerated R Code:\n');
-    console.log(printR(rAst));
+    
+    console.log(JSON.stringify(rAst, null, 2))
+    return printR(rAst);
 }
 
 function execute() {
@@ -17,7 +17,19 @@ function execute() {
     if (args.length < 1) throw new Error('No source file specified.');
 
     const filename = args[0];
-    compile(filename);
+    const output_text = compile(filename);
+
+    const out_filename = filename.slice(
+        Math.max(filename.lastIndexOf('\\'), 0),
+        filename.indexOf('.ts')
+    )
+
+    if (args.length < 2) {
+        console.log('\nGenerated R Code:\n');
+        console.log(output_text);
+    } else {
+        fs.writeFileSync(args[1] + '/' + out_filename, output_text);
+    }
 }
 
 execute();
